527509521ff8ce63be10e35ea8604485279f411b
carloslme <32799711+carloslme@users.noreply.github.com>
2021-09-21 23:58:43 -0500

    feat: add first function to test

---
+++ class-5/nearest.py
@@ -0,0 +1,11 @@
+def nearest_square(num):
+    """Return the nearest perfect square that
+    is less than or equal to num.
+
+    Args:
+        num (int): number integer
+    """
+    root = 0
+    while (root + 1) ** 2 <= num:
+        root += 1
+    return root ** 2
